81f3476fa8f61dcae424ab683eeda8e18cbcf789
akicho8 <akicho8@gmail.com>
2024-12-10 21:11:08 +0900

    [fix] [shogi-home] audit に sha256 :no_check としろと言われるので変更する

--- Casks/shogi-home.rb
+++ Casks/shogi-home.rb
@@ -4,7 +4,7 @@ cask "shogi-home" do
   name "ShogiHome"
 
   version "1.19.2"
-  sha256 "c9791736fb4534d9ebb52a4764338911767c7be5ec63c18980e79442395a6a89"
+  sha256 :no_check
   url "https://github.com/sunfish-shogi/shogihome/releases/download/v1.19.2/release-v1.19.2-mac.zip"
 
   container nested: "ShogiHome-1.19.2-universal.dmg"
