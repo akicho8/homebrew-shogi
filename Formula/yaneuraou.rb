cb41bd18a93ad57c858cd8863641eee1fd8fca49
akicho8 <akicho8@gmail.com>
2024-12-10 20:40:23 +0900

    [fix] [yaneuraou] tr するには to_s しないといけなかった

--- Formula/yaneuraou.rb
+++ Formula/yaneuraou.rb
@@ -3,20 +3,20 @@ class Yaneuraou < Formula
   homepage "https://github.com/yaneurao/YaneuraOu"
 
   version "8.30"
-  url "https://github.com/yaneurao/YaneuraOu/releases/download/v#{version}git/yaneuraou-V#{version.tr(".", "")}-git-mac-all.7z"
+  url "https://github.com/yaneurao/YaneuraOu/releases/download/v#{version}git/yaneuraou-V#{version.to_s.tr(".", "")}-git-mac-all.7z"
   sha256 "3c4a65f0e9018ba6fb745f01239b6aeb4710d42ed60af43599440b3f9c41febe"
 
   def install
     if Hardware::CPU.intel?
       if Hardware::CPU.features.include?(:avx2)
-        prefix.install Dir["YaneuraOu_NNUE_halfKPE9-V#{version.tr(".", "")}Git_APPLEAVX2"]
+        prefix.install Dir["YaneuraOu_NNUE_halfKPE9-V#{version.to_s.tr(".", "")}Git_APPLEAVX2"]
       end
       if Hardware::CPU.features.include?(:sse4_2)
-        prefix.install Dir["YaneuraOu_NNUE_halfKPE9-V#{version.tr(".", "")}Git_APPLESSE42"]
+        prefix.install Dir["YaneuraOu_NNUE_halfKPE9-V#{version.to_s.tr(".", "")}Git_APPLESSE42"]
       end
     end
     if Hardware::CPU.arm?
-      prefix.install Dir["YaneuraOu_NNUE_halfKP256-V#{version.tr(".", "")}Git_APPLEM1"]
+      prefix.install Dir["YaneuraOu_NNUE_halfKP256-V#{version.to_s.tr(".", "")}Git_APPLEM1"]
     end
   end
 end
