a39cbda99ac1e0353d14f1376479d85e3decffc1
akicho8 <akicho8@gmail.com>
2024-12-10 20:36:50 +0900

    [refactor] audio すると書き順で起られたので適当に並べ替えてみる

--- Casks/shogi-home.rb
+++ Casks/shogi-home.rb
@@ -1,11 +1,11 @@
 cask "shogi-home" do
-  version "1.19.2"
-  sha256 "c9791736fb4534d9ebb52a4764338911767c7be5ec63c18980e79442395a6a89"
-
-  url "https://github.com/sunfish-shogi/shogihome/releases/download/v#{version}/release-v#{version}-mac.zip"
-  name "ShogiHome"
   desc "ShogiHome (しょうぎほーむ) は Windows または macOS・Linux で動作する将棋アプリです。公開のソースコードを用いて独自のバージョンを作成して配布が可能です。"
   homepage "https://github.com/sunfish-shogi/shogihome"
+  name "ShogiHome"
+
+  version "1.19.2"
+  sha256 "c9791736fb4534d9ebb52a4764338911767c7be5ec63c18980e79442395a6a89"
+  url "https://github.com/sunfish-shogi/shogihome/releases/download/v#{version}/release-v#{version}-mac.zip"
 
   container nested: "ShogiHome-#{version}-universal.dmg"
   app "ShogiHome.app"
--- Formula/yaneuraou.rb
+++ Formula/yaneuraou.rb
@@ -1,10 +1,10 @@
 class Yaneuraou < Formula
-  version "8.30"
-  sha256 "3c4a65f0e9018ba6fb745f01239b6aeb4710d42ed60af43599440b3f9c41febe"
-  url "https://github.com/yaneurao/YaneuraOu/releases/download/v#{version}git/yaneuraou-V#{version.tr(".", "")}-git-mac-all.7z"
-
   desc "やねうら王は、WCSC29(世界コンピュータ将棋選手権/2019年)、第4回世界将棋AI電竜戦本戦(2023年)などにおいて優勝した世界最強の将棋の思考エンジンです。教育的でUSIプロトコルに準拠しています。"
   homepage "https://github.com/yaneurao/YaneuraOu"
+
+  version "8.30"
+  url "https://github.com/yaneurao/YaneuraOu/releases/download/v#{version}git/yaneuraou-V#{version.tr(".", "")}-git-mac-all.7z"
+  sha256 "3c4a65f0e9018ba6fb745f01239b6aeb4710d42ed60af43599440b3f9c41febe"
 
   def install
     if Hardware::CPU.intel?
