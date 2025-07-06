3ea47ea5df092be08947f769afd86cdfc817d07d
akicho8 <akicho8@gmail.com>
2025-07-06 23:05:19 +0900

    [feat] [shogi-home] 1.24.0

--- Casks/shogi-home.rb
+++ Casks/shogi-home.rb
@@ -3,11 +3,11 @@ cask "shogi-home" do
   homepage "https://github.com/sunfish-shogi/shogihome"
   name "ShogiHome"
 
-  version "1.23.0"
+  version "1.24.0"
   sha256 :no_check
-  url "https://github.com/sunfish-shogi/shogihome/releases/download/v1.23.0/release-v1.23.0-mac.zip"
+  url "https://github.com/sunfish-shogi/shogihome/releases/download/v1.24.0/release-v1.24.0-mac.zip"
 
-  container nested: "ShogiHome-1.23.0-universal.dmg"
+  container nested: "ShogiHome-1.24.0-universal.dmg"
 
   # License: MIT
   # The software is licensed under the MIT License.
